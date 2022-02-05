c547faa7545d79fd789019a4397b0f42cd452a53
SAMIUL98SIAM <samiulsiam89@gmail.com>
2022-02-05 21:05:30 +0600

    1st & 2nd Lab task

---
+++ Task2/Task1/Controllers/PersonController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ind()
+        {
+            var person = new Person()
+            {
+                Name = "Salam",
+                Id = 1,
+                Dob = DateTime.Now
+            };
+            return View(person);
+        }
+
+        public ActionResult Home()
+        {
+            /*
+            ViewBag.name = "Samiul";
+            ViewBag.id = "18-38844-3";
+            ViewBag.dob = "28-01-1998";
+
+            ViewData["name"] = "Mumu";
+            ViewData["id"] = "18-36765-1";
+            ViewData["dob"] = "15-09-1998";
+
+            string[] names = { "Siam", "Akib", "Razib", "Hasib" };
+            ViewBag.names = names;
+            */
+
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 50; i++)
+            {
+                var person = new Person()
+                {
+                    Name = "Persons" + (i + 1),
+                    Id = (i + 1),
+                    Dob = DateTime.Now
+                };
+                persons.Add(person);
+            }
+            
+            return View(persons);
+        }
+    }
+}
---
+++ Task2/Task1/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                products.Add(new Product() { 
+                    Name = "Product No" + (i+1),
+                    Id = i+1
+                });
+
+            }
+            return View(products);
+        }
+    }
+}
